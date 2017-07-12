9034deeba90c585660529fdd27bdd29e9d6e801b
권용근 <kingbbode@zuminternet.com>
2017-07-12 15:53:08 +0900

    저장소 형태 변경으로 인한 초기화

--- spring-boot-starter-chatbot/src/main/java/com/kingbbode/chatbot/autoconfigure/common/enums/BrainRequestType.java
+++
@@ -1,9 +0,0 @@
-package com.kingbbode.chatbot.autoconfigure.common.enums;
-
-/**
- * Created by YG on 2017-01-26.
- */
-public enum BrainRequestType {
-    COMMON,
-    API
-}
--- spring-boot-starter-chatbot/src/main/java/com/kingbbode/chatbot/autoconfigure/common/interfaces/Dispatcher.java
+++
@@ -1,8 +0,0 @@
-package com.kingbbode.chatbot.autoconfigure.common.interfaces;
-
-/**
- * Created by YG on 2017-07-10.
- */
-public interface Dispatcher<T> {
-    void dispatch(T event);
-}
